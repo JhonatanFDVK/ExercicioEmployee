67deaf2112856ec078e030009deec2d9efc3c68f
Jhonatan Silva <jhonatanfdvk@gmail.com>
2024-03-19 21:25:23 -0300

    Classes criadas

---
+++ Entities/OutsoruceEmployee.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioPolimorfismo01.Entities
+{
+    internal class OutsoruceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsoruceEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 110 * AdditionalCharge / 100;
+        }
+    }
+}
